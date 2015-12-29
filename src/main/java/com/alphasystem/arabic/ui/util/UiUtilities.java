31d3a59752d57e5821ef71af1d9766f388c39373
sali <sali2@loyalty.com>
2015-12-29 15:42:50 -0500

    convenience method to wrap a node in scroll pane

--- src/main/java/com/alphasystem/arabic/ui/util/UiUtilities.java
+++ src/main/java/com/alphasystem/arabic/ui/util/UiUtilities.java
@@ -3,9 +3,11 @@ package com.alphasystem.arabic.ui.util;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 
 import static javafx.scene.Cursor.DEFAULT;
 import static javafx.scene.Cursor.WAIT;
+import static javafx.scene.control.ScrollPane.ScrollBarPolicy.AS_NEEDED;
 
 /**
  * @author sali
@@ -20,6 +22,16 @@ public final class UiUtilities {
         changeCursor(node, WAIT);
     }
 
+    public static ScrollPane wrapInScrollPane(Node node) {
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(node);
+        scrollPane.setVbarPolicy(AS_NEEDED);
+        scrollPane.setHbarPolicy(AS_NEEDED);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        return scrollPane;
+    }
+
     private static void changeCursor(Node node, Cursor cursor) {
         if (node != null) {
             Scene scene = node.getScene();
@@ -28,4 +40,5 @@ public final class UiUtilities {
             }
         }
     }
+
 }
