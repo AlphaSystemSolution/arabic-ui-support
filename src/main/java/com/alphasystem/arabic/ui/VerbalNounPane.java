4d3bca905ee3fc6106fd6f4c68cdd6eb5c2b4829
sfali23 <f.syed.ali@gmail.com>
2017-01-06 15:01:03 -0500

    clean up

--- src/main/java/com/alphasystem/arabic/ui/VerbalNounPane.java
+++ src/main/java/com/alphasystem/arabic/ui/VerbalNounPane.java
@@ -16,7 +16,7 @@ public class VerbalNounPane extends ArabicSupportGroupPane<VerbalNoun> {
                 VERBAL_NOUN_V12, VERBAL_NOUN_V13, VERBAL_NOUN_V14, VERBAL_NOUN_V15, VERBAL_NOUN_V27,
                 VERBAL_NOUN_FORM_II, VERBAL_NOUN_FORM_III_V1, VERBAL_NOUN_FORM_III_V2, VERBAL_NOUN_FORM_IV,
                 VERBAL_NOUN_FORM_V, VERBAL_NOUN_FORM_VI, VERBAL_NOUN_FORM_VII, VERBAL_NOUN_FORM_VIII, VERBAL_NOUN_FORM_IX,
-                VERBAL_NOUN_FORM_X, VERBAL_NOUN_FORM_XI});
+                VERBAL_NOUN_FORM_X});
     }
 
     private VerbalNounPane(int numOfColumns, VerbalNoun[] srcValues) {
