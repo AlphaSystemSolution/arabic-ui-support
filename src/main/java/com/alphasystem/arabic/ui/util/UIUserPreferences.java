1f007b81dc55d9c82578afa58ac2d6a81f467835
sfali23 <f.syed.ali@gmail.com>
2017-01-08 22:45:57 -0500

    separating logic to load font into separate class

--- src/main/java/com/alphasystem/arabic/ui/util/UIUserPreferences.java
+++ src/main/java/com/alphasystem/arabic/ui/util/UIUserPreferences.java
@@ -5,7 +5,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 
-import java.util.List;
 import java.util.prefs.Preferences;
 
 /**
@@ -14,11 +13,6 @@ import java.util.prefs.Preferences;
 public abstract class UIUserPreferences extends GenericPreferences {
 
     private static final String FONT_NODE_NAME = "FONT_PREFERENCES";
-    private static final String KFGQPC_UTHMAN_TAHA_NASKH = "KFGQPC Uthman Taha Naskh";
-    private static final String TRADITIONAL_ARABIC = "Traditional Arabic";
-    private static final String ARABIC_TYPESETTING = "Arabic Typesetting";
-    private static final String ARIAL = "Arial";
-    private static final String DEFAULT_FONT_NAME = getDefaultArabicFontName();
     private static final String ARABIC_FONT_NAME_KEY = "arabicFontName";
     private static final String ARABIC_FONT_SIZE = "arabicFontSize";
     private static final String ENGLISH_FONT_NAME = "englishFontName";
@@ -33,56 +27,43 @@ public abstract class UIUserPreferences extends GenericPreferences {
     }
 
     public String getArabicFontName() {
-        return getFontNode().get(ARABIC_FONT_NAME_KEY, DEFAULT_FONT_NAME);
+        return getFontNode().get(ARABIC_FONT_NAME_KEY, FontUtilities.getDefaultArabicFontName());
     }
 
     public void setArabicFontName(String name) {
         getFontNode().put(ARABIC_FONT_NAME_KEY, name);
     }
 
-    public long getArabicFontSize(){
-        return getFontNode().getLong(ARABIC_FONT_SIZE, 20);
+    public long getArabicFontSize() {
+        return getFontNode().getLong(ARABIC_FONT_SIZE, FontUtilities.DEFAULT_ARABIC_FONT_SIZE);
     }
 
-    public void setArabicFontSize(long size){
+    public void setArabicFontSize(long size) {
         getFontNode().putLong(ARABIC_FONT_SIZE, size);
     }
 
-    public String getEnglishFontName(){
-        return getFontNode().get(ENGLISH_FONT_NAME, "Candara");
+    public String getEnglishFontName() {
+        return getFontNode().get(ENGLISH_FONT_NAME, FontUtilities.getDefaultEnglishFont());
     }
 
-    public void setEnglishFontName(String name){
+    public void setEnglishFontName(String name) {
         getFontNode().put(ENGLISH_FONT_NAME, name);
     }
 
-    public long getEnglishFontSize(){
-        return getFontNode().getLong(ENGLISH_FONT_SIZE, 12);
+    public long getEnglishFontSize() {
+        return getFontNode().getLong(ENGLISH_FONT_SIZE, FontUtilities.DEFAULT_ENGLISH_FONT_SIZE);
     }
 
-    public void setEnglishFontSize(long size){
+    public void setEnglishFontSize(long size) {
         getFontNode().putLong(ENGLISH_FONT_SIZE, size);
     }
 
-    public Font getArabicFont(){
+    public Font getArabicFont() {
         return Font.font(getArabicFontName(), FontWeight.NORMAL, FontPosture.REGULAR, getArabicFontSize());
     }
 
-    public Font getEnglishFont(){
+    public Font getEnglishFont() {
         return Font.font(getEnglishFontName(), FontWeight.NORMAL, FontPosture.REGULAR, getEnglishFontSize());
     }
 
-    private static String getDefaultArabicFontName() {
-        final List<String> families = Font.getFamilies();
-        if (families.contains(KFGQPC_UTHMAN_TAHA_NASKH)) {
-            return KFGQPC_UTHMAN_TAHA_NASKH;
-        }
-        if (families.contains(TRADITIONAL_ARABIC)) {
-            return TRADITIONAL_ARABIC;
-        }
-        if (families.contains(ARABIC_TYPESETTING)) {
-            return ARABIC_TYPESETTING;
-        }
-        return ARIAL;
-    }
 }
